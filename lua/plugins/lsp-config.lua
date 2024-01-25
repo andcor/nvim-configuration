8d60c8774063a140f8a22d358259132a6a25493e
Andreas Corneliussen <aco@skytem.com>
2024-01-25 09:51:30 +0100

    Added lazygit and changed some keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,11 +35,21 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities
       })
+      lspconfig.svlangserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.svls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.verible.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.code_action, {})
     end,
   },
 }
